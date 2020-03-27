4d4494de0f184f1456532c29ae903440a64b692c
Anton Strogonoff <anton@strogonoff.name>
2020-03-27 20:16:12 +0900

    fix: Clear placeholder secret key

--- gd/settings.py
+++ gd/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gq-#c+%&%os4_!(&ra8_ogq67k86qbguhuqcb7td0uiurk1fs$'
+# SECRET_KEY = '<enter a really long random string here>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
