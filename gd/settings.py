cf753e19e5673e51333178c047f33d3273e0c557
Anton Strogonoff <anton@strogonoff.name>
2020-03-27 20:16:24 +0900

    chore: Set DEBUG = False by default

--- gd/settings.py
+++ gd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '<enter a really long random string here>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['findthegender.com']
 
